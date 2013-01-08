1993352ee37e3c7363d06bab23dbeca42215b007
golden_jubilee_cp <steffanv@users.noreply.github.com>
2013-01-08 18:59:14 +0000

    some minor improvements

--- MCDA/Util.cs
+++ MCDA/Util.cs
@@ -13,7 +13,25 @@ namespace MCDA.Model
 
         public static string GetPropertyName<T>(Expression<Func<T>> expression)
         {
+            object o = expression.Body as MemberExpression;
+
             return (expression.Body as MemberExpression).Member.Name;
+        }
+
+        public static string GetPropertyValue<T>(Expression<Func<T>> expression)
+        {
+            return GetValue(expression.Body as MemberExpression).ToString();
+        }
+
+        private static object GetValue(MemberExpression member)
+        {
+            var objectMember = Expression.Convert(member, typeof(object));
+
+            var getterLambda = Expression.Lambda<Func<object>>(objectMember);
+
+            var getter = getterLambda.Compile();
+
+            return getter();
         }
 
         //seriously ESRI? I need the ArcGIS Military Analyst? 

d151cf4fd30befb47619b8b823c5c26cfde2f791
golden_jubilee_cp <steffanv@users.noreply.github.com>
2014-01-30 23:11:29 +0000

    tests, standardization

--- MCDA/IStandardizationStrategy.cs
+++ MCDA/IStandardizationStrategy.cs
@@ -6,10 +6,25 @@ using System.Data;
 
 namespace MCDA.Model
 {
+    /// <summary>
+    /// Represents a strategy to standardize a list of numerical values.
+    /// </summary>
     public interface IStandardizationStrategy
     {
+        /// <summary>
+        /// Transforms a DataColumn in place.
+        /// </summary>
+        /// <param name="column"></param>
+        /// <param name="benefitCriterion"></param>
         void Transform(DataColumn column, bool benefitCriterion = true);
 
-        double? Transform(IList<double> data, double actualValue, bool benefitCriterion = false);
+        /// <summary>
+        /// Transforms a list in place.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="actualValue"></param>
+        /// <param name="benefitCriterion"></param>
+        /// <returns></returns>
+        double? Transform(IEnumerable<double> data, double actualValue, bool benefitCriterion = false);
     }
 }
--- MCDA/MaximumScoreStandardizationStrategy.cs
+++ MCDA/MaximumScoreStandardizationStrategy.cs
@@ -9,7 +9,7 @@ namespace MCDA.Model
 {
     internal sealed class MaximumScoreStandardizationStrategy : IStandardizationStrategy
     {
-        public double? Transform(IList<double> data, double actualValue, bool benefitCriterion = false)
+        public double? Transform(IEnumerable<double> data, double actualValue, bool benefitCriterion = false)
         {
             double maxValue = data.Max();
 
@@ -24,23 +24,28 @@ namespace MCDA.Model
 
         public void Transform(DataColumn column, bool benefitCriterion = true)
         {
-            // check other types and empty column
-            double maxValue = (double)column.Table.Compute("max(" + column.ColumnName + ")", String.Empty);
+            if (!column.IsNumeric() || column.Table.Rows.Count <= 0)
+                return;
+          
+            dynamic maxValue = column.Table.Compute("max(" + column.ColumnName + ")", String.Empty);
 
-            int columnIndex = column.Ordinal;
+            if (maxValue == 0)
+                return;
+
+            int columnIndex = column.Ordinal;  
 
             foreach (DataRow currentDataRow in column.Table.Rows)
             {
                 if (benefitCriterion)
                 {
-                    if (maxValue != 0)
-                    {
-                        currentDataRow[columnIndex] = (double)currentDataRow.ItemArray[columnIndex] / maxValue;
-                    }
+                    dynamic currentValue = currentDataRow.ItemArray[columnIndex];
+                    currentDataRow[columnIndex] = currentValue / maxValue;
                 }
+
                 else
                 {
-                    currentDataRow[columnIndex] = 1d - ((double)currentDataRow.ItemArray[columnIndex] / maxValue);
+                    dynamic currentValue = currentDataRow.ItemArray[columnIndex];
+                    currentDataRow[columnIndex] = 1d - (currentValue / maxValue);
                 }
             }
         }
